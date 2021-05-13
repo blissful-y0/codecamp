809c53028acd70ea5d5bc2383a33df591616c3fc
blissful-y0 <71304578+blissful-y0@users.noreply.github.com>
2021-05-13 16:51:28 +0900

    5/13 work

---
+++ class/pages/async-await/index.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function AsyncAwaitPage() {
+  const [titleFnfo, setTitleInfo] = useState("전달받은 정보가 표기됩니다.");
+  const [content, setCotents] = useState("");
+  const [updatedAt, setUpdatedAt] = useState("");
+  const [count, setCount] = useState(1);
+
+  async function handleClickGetPost() {
+    const datalength = await axios.get("https://koreanjson.com/posts");
+    const data = await axios.get(`https://koreanjson.com/posts/${count}`);
+    let date = new Date(data.data.updatedAt);
+    let dateObject = {
+      year: date.getFullYear(),
+      month: String(date.getMonth() + 1).padStart(2, "0"),
+      date: date.getDate() - 1,
+    };
+    let countIndex = count + 1;
+    if (countIndex > datalength.data.length) {
+      countIndex = 0;
+    }
+    setTitleInfo(data.data.title);
+    setCotents(data.data.content);
+    setUpdatedAt(`${dateObject.year}-${dateObject.month}-${dateObject.date}`);
+    setCount(countIndex);
+  }
+
+  return (
+    <>
+      제목: <div>{titleFnfo}</div>
+      <br></br>
+      내용: <div>{content}</div>
+      <br></br>
+      작성일: <div>{updatedAt}</div>
+      <button onClick={handleClickGetPost}>게시물 가져오기</button>
+    </>
+  );
+}
---
+++ class/pages/mutation/index.js
@@ -0,0 +1,107 @@
+import { useMutation, gql } from "@apollo/client";
+import { useState } from "react";
+
+export default function MutationPage() {
+  const CREATE_BOARD = gql`
+    mutation variableType(
+      $writer: String
+      $password: String
+      $title: String
+      $contents: String
+    ) {
+      createBoard(
+        writer: $writer
+        password: $password
+        title: $title
+        contents: $contents
+      ) {
+        message
+      }
+    }
+  `;
+
+  const [data, setData] = useState({
+    writer: "",
+    password: "",
+    title: "",
+    contents: "",
+  });
+
+  const [createBoardMutation] = useMutation(CREATE_BOARD);
+  // const [writer, setWriter] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [contents, setContents] = useState("");
+
+  async function onClickPost() {
+    try {
+      const result = await createBoardMutation({
+        variables: { ...data },
+      });
+      alert(result.data.createBoard.message);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  const onChangeInput = (event) => {
+    const userData = { ...data, [event.target.name]: event.target.value };
+    setData(userData);
+    console.log(userData);
+  };
+
+  // const onChangeWriter = (event) => {
+  //   let temp = event.target.value;
+  //   // setWriter(temp);
+  // };
+  // const onChangePassword = (event) => {
+  //   let temp = event.target.value;
+  //   // setPassword(temp);
+  // };
+  // const onChangedTitle = (event) => {
+  //   let temp = event.target.value;
+  //   // setTitle(temp);
+  // };
+  // const onChangeContent = (event) => {
+  //   let temp = event.target.value;
+  //   // setContents(temp);
+  // };
+
+  return (
+    <>
+      작성자:{" "}
+      <input
+        type="text"
+        name="writer"
+        placeholder="이름을 입력해주세요"
+        onChange={onChangeInput}
+      ></input>
+      <br></br>
+      비밀번호:{" "}
+      <input
+        type="text"
+        name="password"
+        placeholder="비밀번호를 입력해주세요"
+        onChange={onChangeInput}
+      ></input>
+      <br></br>
+      제목:
+      <input
+        type="text"
+        name="title"
+        placeholder="제목을 입력해주세요"
+        onChange={onChangeInput}
+      ></input>
+      <br></br>
+      내용:{" "}
+      <input
+        type="text"
+        name="contents"
+        placeholder="내용을 입력해주세요"
+        onChange={onChangeInput}
+      ></input>
+      <br></br>
+      <button onClick={onClickPost}>게시물 등록하기</button>
+    </>
+  );
+}
