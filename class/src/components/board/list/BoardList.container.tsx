c5d667e2afabad0921f07853768419a9ec39bc0a
blissful-y0 <blissful.y0@gmail.com>
2021-05-20 09:46:28 +0900

    Update class

---
+++ class/src/components/board/list/BoardList.container.tsx
@@ -0,0 +1,27 @@
+import { useRouter } from "next/router";
+import { useQuery } from "@apollo/client";
+import { FETCH_BOARDS } from "./BoardList.quries";
+import RenderUI from '../list/BoardList.presenter'
+
+export default function BoardPage() {
+  const router = useRouter();
+
+  const { data, loading, error } = useQuery(FETCH_BOARDS);
+  if (loading) {
+    return "";
+  }
+  if (error) {
+    return "에러가 발생했습니다!";
+  }
+
+  const onClickTitle = (_number) => (event) => {
+    router.push(`/list/${_number}`);
+  };
+
+
+  return (
+    <>
+    <RenderUI data={data} onClickTitle={onClickTitle} />
+  </>
+  );
+}
---
+++ class/src/components/board/list/BoardList.quries.ts
@@ -0,0 +1,14 @@
+import { gql } from "@apollo/client";
+
+export const FETCH_BOARDS = gql`
+  query {
+    fetchBoards {
+      number
+      writer
+      title
+      contents
+      like
+      createdAt
+    }
+  }
+`;
