7a357b81726b9a7ca9c2290158083725449d68d9
blissful-y0 <blissful.y0@gmail.com>
2021-06-07 21:09:09 +0900

    layout fixed

--- freeboard_frontend/src/commons/components/layout/header/header.presenter.tsx
+++ freeboard_frontend/src/commons/components/layout/header/header.presenter.tsx
@@ -31,7 +31,7 @@ export default function Header() {
           </LoginSignInWrapper>
         </TitleWrapper>
         <Slider />
-        {isNavigation && <Navigation />}
+        <Navigation />
       </LayoutContext.Provider>
     </>
   );
