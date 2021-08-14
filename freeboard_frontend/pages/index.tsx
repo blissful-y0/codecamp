2814b5afeee55366e4979ddf488f6e694bee07f0
blissful-y0 <blissful.y0@gmail.com>
2021-08-14 22:05:19 +0900

    update: 중고마켓 다듬기

--- freeboard_frontend/pages/index.tsx
+++ freeboard_frontend/pages/index.tsx
@@ -4,58 +4,5 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   const kakaoKey = '7b361276d43366bba8247ce0a8956924';
 
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Melong.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <span className={styles.logo}></span>
-        </a>
-      </footer>
-    </div>
-  );
+  return <div>INDEX 공사 중 </div>;
 }
--- freeboard_frontend/src/market/reply/replyAnswer/replyansweritem.presenter.tsx
+++ freeboard_frontend/src/market/reply/replyAnswer/replyansweritem.presenter.tsx
@@ -147,7 +147,6 @@ export default function AnswerUI({
           </UpdateCommentWrapper>
         </UpdateWrapper>
       )}
-
       {open && (
         <ReplyWrite
           replyId={replyId}
